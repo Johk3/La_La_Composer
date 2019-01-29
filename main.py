7b08bc8d12fc00a4fdca982953b861e34829bfde
johk <user3610@protonmail.com>
2019-01-29 19:09:35 +0200

    All notes have been tokenized and are almost ready to be trained on

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
-import midi
 from music21 import converter,instrument, duration, chord, stream # or import *
-s = converter.parse('midis/The_Godfather.mid')
-allnotes = []
-allquarterlengths = []
-checkers = {}
-i = 0
-for el in s.recurse():
-    dur = el.duration.quarterLength
-    note = str(el).split(" ")[-1]
-    note = note.replace(">", "")
-    allnotes.append(note)
+from keras.preprocessing.text import text_to_word_sequence
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.text import Tokenizer
+from os import walk
 
-    if dur > 10:
-        el.duration.quarterLength = 5
 
-    if dur < 0.4:
-        el.duration.quarterLength = 0.2
+allnotesl = []
+notes = []
 
-    if 'Instrument' in el.classes: # or 'Piano'
-        el.activeSite.replace(el, instrument.Flute())
+midis = []
+for (dirpath, dirnames, filenames) in walk("midis"):
+    midis.extend(filenames)
+    break
 
-    if not checkers[note]:
-        checkers[note] = i
-        i+=1
+for midi in midis:
+    s = converter.parse('midis/{}'.format(midi))
+    print("Preparing {}".format(midi))
 
-    allquarterlengths.append(el.duration.quarterLength)
+    i = 0
+    for el in s.recurse():
+        dur = el.duration.quarterLength
+        note = str(el).split(" ")[-1]
+        note = note.replace(">", "")
 
-print allnotes
-print allquarterlengths
-print len(allnotes), len(allquarterlengths)
-s.write('midi', 'example.mid')
+        if dur > 10:
+            el.duration.quarterLength = 5
+
+        if dur <= 0.5:
+            el.duration.quarterLength = 0.5
+
+        if 'Instrument' in el.classes: # or 'Piano'
+            el.activeSite.replace(el, instrument.Harp())
+
+        notelength = [note, el.duration.quarterLength]
+        allnotesl.append(notelength)
+
+    # s.write('midi', 'example.mid')
+
+
+    for notel in allnotesl:
+        if isinstance(notel[0], str):
+            notes.append(str(notel))
+
+    t = Tokenizer()
+    # fit the tokenizer on the documents
+    oof = t.fit_on_texts(notes)
+
+
+# summarize what was learned
+print("Length, ", t.document_count)
+print("Allnotes length: ", len(allnotesl))
+# print(t.word_index)
+print(t.index_word)
+
