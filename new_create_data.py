71fd70583f206e8f89dd23ad0d64831c7d729032
johk <user3610@protonmail.com>
2019-07-29 10:20:23 +0300

    Able to make clear data out of the decoded midis into the file lala.txt

--- new_create_data.py
+++ new_create_data.py
@@ -1,7 +1,7 @@
 from __future__ import division, print_function, absolute_import
 from PIL import Image
 import numpy as np
-from music21 import converter,instrument, duration, chord, stream, midi, note # or import *
+from music21 import converter,instrument, duration, chord, stream, midi, note, ElementWrapper # or import *
 from keras.preprocessing.text import text_to_word_sequence
 from keras.preprocessing.text import one_hot
 from keras.preprocessing.text import Tokenizer
@@ -53,6 +53,7 @@ class Main:
                 # s.write('midi', 'example.mid')
 
                 for notel in allnotesl:
+                    #print("{} = {}".format(type(notel[0]), type(notel[1])))
                     notelstr.append(str(notel[1]))
                     notes.append(str(notel[0]))
 
@@ -114,27 +115,45 @@ class Main:
                 for y in range(img.size[1]):
                     if pixels[x,y] == (255, 255, 255):
                         midis[notes[x]] = length[y]
-        print(midis)
+
         print("Done decoding...")
         return midis
 
-    def buildMidis(self):
-        # Build midis here
-        pass
-
+    def buildMidis(self, midis):
+        with open("lala.txt", "w+") as file:
+            for n, length in midis.items():
+                file.write("{} {}\n".format(n, length))
+        #     check = True
+        #     s = stream.Stream()
+        #     n = ElementWrapper(n)
+        #     try:
+        #         length = float(length)
+        #     except Exception:
+        #         length = 1.0
+        #     print(n, length)
+        #     # if "/" in length:
+        #     #     length = float(length)
+        #     try:
+        #         n = note.Note(n)
+        #     except Exception as e:
+        #         check = False
+        #     try:
+        #         n.quarterLength = length
+        #     except Exception as e:
+        #         check = False
+        #     if check != False:
+        #         s.append(n)
+        #
+        # mf = midi.translate.streamToMidiFile(s)
+        # mf.open('outmidis/midi.mid', 'wb')
+        # mf.write()
+        # mf.close()
+        print("Done!")
 
 
 if __name__ == "__main__":
-    s = stream.Stream()
-    n = note.Note('g#')
-    n.quarterLength = .5
-    s.repeatAppend(n, 4)
-    mf = midi.translate.streamToMidiFile(s)
-    mf.open('outmidis/midi.mid', 'wb')
-    mf.write()
-    mf.close()
-    exit(0)
     engine = Main()
     note_data, len_data = engine.get_notes()
     notes, length = engine.make_data(note_data, len_data)
-    engine.decode(notes, length)
+    midis = engine.decode(notes, length)
+    engine.buildMidis(midis)
